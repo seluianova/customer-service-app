968313d849fc13cf23a144453f806790dff2fe6f
tory <my.login.was.busy@gmail.com>
2025-01-03 23:00:20 +0100

    Not found page

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,14 @@ import {
 import HomePage from "./pages/HomePage.tsx";
 import MainLayout from "./layouts/MainLayout.tsx";
 import AddCustomerPage from "./pages/AddCustomerPage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<MainLayout />}>
             <Route index element={<HomePage />} />
             <Route path="/add" element={<AddCustomerPage />} />
+            <Route path="*" element={<NotFoundPage />} />
         </Route>
     )
 );
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+
+const NotFoundPage = () => {
+    return (
+        <section className="text-center flex flex-col justify-center items-center py-6">
+            <div className="container-xl lg:container m-auto">
+                <div className="grid grid-cols-1 p-4 rounded-lg">
+                    <div className="bg-gray-100 p-6 rounded-lg shadow-md">
+                        <ExclamationTriangleIcon className="w-28 p-4 mx-auto text-yellow-500"/>
+                        <h1 className="text-xl font-bold mb-4">404 Not Found</h1>
+                        <p className="mb-5">This page does not exist</p>
+                        <Link
+                            to="/"
+                            className="inline-block text-white rounded-lg px-4 py-2 bg-sky-700 hover:bg-gray-800"
+                        >Go Back
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default NotFoundPage;
