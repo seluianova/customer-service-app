f6103d910316bb095b2c3dcd2c8ab5fa5c3d3f77
tory <my.login.was.busy@gmail.com>
2025-01-03 20:40:46 +0100

    Fix vulnerabilities

--- src/components/CustomerFilter.tsx
+++ src/components/CustomerFilter.tsx
@@ -1,16 +1,22 @@
-import Button from "./Button.tsx";
+import { useState } from "react";
 
 const CustomerFilter = () => {
+    const [ filter, setFilter ] = useState("");
+    const [ lastName, setLastName ] = useState("");
+
     return (
         <div className="flex gap-4 w-96 mt-4 mb-6">
             <input
-                type="text"
+                type="search"
                 placeholder="Enter last name..."
-                // value={filter}
-                // onChange={(e) => setFilter(e.target.value)}
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
                 className="px-4 py-2 border border-gray-300 rounded-md w-full"
             />
-            <Button title="Search" href="tbd.html"/>
+            <button
+                className="inline-block text-white rounded-lg px-4 py-2 bg-sky-700 hover:bg-gray-800"
+                onClick={() => setFilter(lastName)}>Search
+            </button>
         </div>
     );
 };
